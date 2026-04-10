895df6693c213901e8b8d50f02d6d67423e1b955
Andrew Hugon <139385540+Endraou@users.noreply.github.com>
2026-04-10 12:54:09 +0200

    add clickable door for scene loading

---
+++ Assets/Scripts/PlanetOne/DoorInteraction.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public GlobalSceneLoader scriptDeChargement;
+
+    void OnMouseDown()
+    {
+        Debug.Log("Porte cliquée");
+        scriptDeChargement.LoadScene("Planet 2 - AOS&P");
+    }
+}
